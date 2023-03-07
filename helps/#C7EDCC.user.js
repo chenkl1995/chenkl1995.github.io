fb8cece2500aad17e4518c93c8a23c5b6309a947
MornSun <chenkl1995@gmail.com>
2023-03-08 00:04:22 +0800

    update

--- helps/#C7EDCC.user.js
+++ helps/#C7EDCC.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         #C7EDCC
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
+// @updateURL    https://github.com/chenkl1995/chenkl1995.github.io/tree/master/helps/%23C7EDCC.user.js
+// @downloadURL  https://github.com/chenkl1995/chenkl1995.github.io/tree/master/helps/%23C7EDCC.user.js
 // @description  https://greasyfork.org/zh-CN;https://github.com/quoid/userscripts#api;
 // @author       chenkl1995
-// @updateURL    https://github.com/chenkl1995/chenkl1995.github.io/tree/master/helps/%23C7EDCC.user.js
 // @match        *://*/*
 // @exclude-match:
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
